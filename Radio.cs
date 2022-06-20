deb6af93cb68c3a623923913e9947fa705d1059c
Александр Мансуров <stud126601@vyatsu.ru>
2022-06-20 21:36:52 +0300

    Добавьте файлы проекта.

---
+++ Radio.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zachet
+{
+    // В репозитории создать ветку dev сделать коомит в ветку dev класса Radio
+    // В репозитории создать ветру test сделать коомит в ветку test класса UnitTest1
+    // Слить ветку test в ветку dev
+    // Слить ветку dev в master
+    // push в репизиторрий на github
+    // отправить ссылку на репизиторий 
+    
+    public class Radio
+    {
+        private int StationNum, Volume, SetNumber;
+        public Radio()
+            : this(0, 0)
+        { }
+        public Radio(int StationNum, int Volume)
+        {
+            this.StationNum = StationNum;
+            this.Volume = Volume;
+        }
+        public int StationNumber
+        {
+            get => StationNum;
+            set => StationNum = value;
+        }
+        public int Vol
+        {
+            get => Volume;
+            set => Volume = value;
+        }
+        public void StationNext()
+        {
+            if (this.StationNum == 9) this.StationNum = 0;
+            else this.StationNum++;
+        }
+        public void StationPrev()
+        {
+            if (this.StationNum == 0) this.StationNum = 9;
+            else this.StationNum--;
+        }
+
+        public void SetNum(int SetNumber)
+        {
+            this.SetNumber = SetNumber;
+            this.StationNumber = SetterNum;
+        }
+        public int SetterNum
+        {
+            get => SetNumber;
+            set
+            {
+                if (SetNumber < 0 || SetNumber > 9)
+                {
+                    if (SetNumber < 0)
+                    {
+                        while (SetNumber < 0) SetNumber = SetNumber + 10;
+                    }
+                    else if (SetNumber > 9)
+                    {
+                        while (SetNumber > 9) SetNumber = SetNumber - 10;
+                    }
+                    SetNumber = value;
+                }
+                else SetNumber = value;
+            }
+        }
+        public void UpVolume()
+        {
+            if (this.Volume < 10) this.Volume++;
+        }
+        public void DownVolume()
+        {
+            if (this.Volume > 0) this.Volume--;
+        }
+    }
+}
---
+++ UnitTestRadio/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Zachet;
+
+namespace UnitTestRadio
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void CheckMaxStation()
+        {
+            int MaxStation = 9;
+            int expected = 9;
+            Zachet.Radio FirstRadio = new Zachet.Radio();
+            FirstRadio.SetNum(MaxStation);
+            int actual = FirstRadio.StationNumber;
+            Assert.AreEqual(expected, actual, 0, "Not correct station!");
+        }
+        [TestMethod]
+        public void CheckMinStation()
+        {
+            int MinStation = 1;
+            int expected = 1;
+            Zachet.Radio FirstRadio = new Zachet.Radio();
+            FirstRadio.SetNum(MinStation);
+            int actual = FirstRadio.StationNumber;
+            Assert.AreEqual(expected, actual, 0, "Not correct station!");
+        }
+        [TestMethod]
+        public void CheckUpStation()
+        {
+            int CurrentStation = 1;
+            int expected = 3;
+            Zachet.Radio FirstRadio = new Zachet.Radio();
+            FirstRadio.SetNum(CurrentStation);
+            FirstRadio.StationNext();
+            FirstRadio.StationNext();
+            int actual = FirstRadio.StationNumber;
+            Assert.AreEqual(expected, actual, 0, "Not correct station!");
+        }
+        [TestMethod]
+        public void CheckDownStation()
+        {
+            int CurrentStation = 1;
+            int expected = 9;
+            Zachet.Radio FirstRadio = new Zachet.Radio();
+            FirstRadio.SetNum(CurrentStation);
+            FirstRadio.StationPrev();
+            FirstRadio.StationPrev();
+            int actual = FirstRadio.StationNumber;
+            Assert.AreEqual(expected, actual, 0, "Not correct station!");
+        }
+        [TestMethod]
+        public void CheckUpVolume()
+        {
+            int CurrentVolume = 9;
+            int expected = 10;
+            Zachet.Radio FirstRadio = new Zachet.Radio(0, CurrentVolume);
+            FirstRadio.UpVolume();
+            FirstRadio.UpVolume();
+            int actual = FirstRadio.Vol;
+            Assert.AreEqual(expected, actual, 0, "Not correct Volume!");
+        }
+        [TestMethod]
+        public void CheckDownVolume()
+        {
+            int CurrentVolume = 1;
+            int expected = 0;
+            Zachet.Radio FirstRadio = new Zachet.Radio(0, CurrentVolume);
+            FirstRadio.DownVolume();
+            FirstRadio.DownVolume();
+            int actual = FirstRadio.Vol;
+            Assert.AreEqual(expected, actual, 0, "Not correct Volume!");
+        }
+    }
+}
